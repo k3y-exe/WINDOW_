05f3956c5d42b7d3b8a9d85d79ec08cea05b1507
k3y <126290511+k3y-exe@users.noreply.github.com>
2026-01-08 14:32:46 +0100

    Update param name scheme

--- window.h
+++ window.h
@@ -19,8 +19,6 @@ typedef struct WINDOW_Window {
 	int width;
 	int height;
 	int shouldClose;
-
-	/* Backend-specific handle */
 	void *handle;
 } WINDOW_Window;
 
@@ -50,35 +48,35 @@ typedef struct WINDOW_X11Window {
 
 static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int p_width, int p_height)
 {
-	WINDOW_X11Window *p_x11;
-	p_x11 = (WINDOW_X11Window*)malloc(sizeof(WINDOW_X11Window));
-	if (!p_x11)
+	WINDOW_X11Window *x11Window;
+	x11Window = (WINDOW_X11Window*)malloc(sizeof(WINDOW_X11Window));
+	if (!x11Window)
 		return 0;
 
-	p_x11->display = XOpenDisplay(NULL);
-	if (!p_x11->display) {
-		free(p_x11);
+	x11Window->display = XOpenDisplay(NULL);
+	if (!x11Window->display) {
+		free(x11Window);
 		return 0;
 	}
 
-	p_x11->screen = DefaultScreen(p_x11->display);
+	x11Window->screen = DefaultScreen(x11Window->display);
 
-	p_x11->window = XCreateSimpleWindow(
-		p_x11->display,
-		RootWindow(p_x11->display, p_x11->screen),
+	x11Window->window = XCreateSimpleWindow(
+		x11Window->display,
+		RootWindow(x11Window->display, x11Window->screen),
 		0, 0,
 		(unsigned int)p_width,
 		(unsigned int)p_height,
 		1,
-		BlackPixel(p_x11->display, p_x11->screen),
-		WhitePixel(p_x11->display, p_x11->screen)
+		BlackPixel(x11Window->display, x11Window->screen),
+		WhitePixel(x11Window->display, x11Window->screen)
 	);
 
-	XStoreName(p_x11->display, p_x11->window, p_title);
+	XStoreName(x11Window->display, x11Window->window, p_title);
 
 	XSelectInput(
-		p_x11->display,
-		p_x11->window,
+		x11Window->display,
+		x11Window->window,
 		ExposureMask |
 		KeyPressMask |
 		KeyReleaseMask |
@@ -88,12 +86,12 @@ static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int
 		StructureNotifyMask
 	);
 
-	p_x11->wmDelete = XInternAtom(p_x11->display, "WM_DELETE_WINDOW", False);
-	XSetWMProtocols(p_x11->display, p_x11->window, &p_x11->wmDelete, 1);
+	x11Window->wmDelete = XInternAtom(x11Window->display, "WM_DELETE_WINDOW", False);
+	XSetWMProtocols(x11Window->display, x11Window->window, &x11Window->wmDelete, 1);
 
-	XMapWindow(p_x11->display, p_x11->window);
+	XMapWindow(x11Window->display, x11Window->window);
 
-	p_window->handle = p_x11;
+	p_window->handle = x11Window;
 	p_window->width = p_width;
 	p_window->height = p_height;
 	p_window->shouldClose = 0;
@@ -103,17 +101,16 @@ static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int
 
 static void WINDOW_WindowPoll(WINDOW_Window *p_window)
 {
-	WINDOW_X11Window *p_x11;
+	WINDOW_X11Window *x11Window;
 	XEvent event;
-	p_x11 = (WINDOW_X11Window*)p_window->handle;
+	x11Window = (WINDOW_X11Window*)p_window->handle;
 
-	while (XPending(p_x11->display)) {
-		XNextEvent(p_x11->display, &event);
+	while (XPending(x11Window->display)) {
+		XNextEvent(x11Window->display, &event);
 
 		if (event.type == ClientMessage) {
-			if ((Atom)event.xclient.data.l[0] == p_x11->wmDelete) {
+			if ((Atom)event.xclient.data.l[0] == x11Window->wmDelete)
 				p_window->shouldClose = 1;
-			}
 		} else if (event.type == ConfigureNotify) {
 			p_window->width = event.xconfigure.width;
 			p_window->height = event.xconfigure.height;
@@ -123,14 +120,14 @@ static void WINDOW_WindowPoll(WINDOW_Window *p_window)
 
 static void WINDOW_WindowDestroy(WINDOW_Window *p_window)
 {
-	WINDOW_X11Window *p_x11;
-	p_x11 = (WINDOW_X11Window*)p_window->handle;
-	if (!p_x11)
+	WINDOW_X11Window *x11Window;
+	x11Window = (WINDOW_X11Window*)p_window->handle;
+	if (!x11Window)
 		return;
 
-	XDestroyWindow(p_x11->display, p_x11->window);
-	XCloseDisplay(p_x11->display);
-	free(p_x11);
+	XDestroyWindow(x11Window->display, x11Window->window);
+	XCloseDisplay(x11Window->display);
+	free(x11Window);
 	p_window->handle = NULL;
 }
 
@@ -157,17 +154,17 @@ typedef struct WINDOW_Win32Window {
 
 static LRESULT CALLBACK WINDOW_WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	WINDOW_Window *p_window;
-	p_window = (WINDOW_Window*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+	WINDOW_Window *window;
+	window = (WINDOW_Window*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
 
 	switch (msg) {
 	case WM_CLOSE:
-		if (p_window) p_window->shouldClose = 1;
+		if (window) window->shouldClose = 1;
 		return 0;
 	case WM_SIZE:
-		if (p_window) {
-			p_window->width = LOWORD(lParam);
-			p_window->height = HIWORD(lParam);
+		if (window) {
+			window->width = LOWORD(lParam);
+			window->height = HIWORD(lParam);
 		}
 		break;
 	}
@@ -177,42 +174,42 @@ static LRESULT CALLBACK WINDOW_WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARA
 
 static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int p_width, int p_height)
 {
-	WINDOW_Win32Window *p_win;
-	p_win = (WINDOW_Win32Window*)malloc(sizeof(WINDOW_Win32Window));
-	if (!p_win)
+	WINDOW_Win32Window *win32Window;
+	win32Window = (WINDOW_Win32Window*)malloc(sizeof(WINDOW_Win32Window));
+	if (!win32Window)
 		return 0;
 
-	p_win->hInstance = GetModuleHandle(NULL);
-	p_win->wc.style = CS_HREDRAW | CS_VREDRAW;
-	p_win->wc.lpfnWndProc = WINDOW_WinProc;
-	p_win->wc.cbClsExtra = 0;
-	p_win->wc.cbWndExtra = 0;
-	p_win->wc.hInstance = p_win->hInstance;
-	p_win->wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	p_win->wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	p_win->wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	p_win->wc.lpszMenuName = NULL;
-	p_win->wc.lpszClassName = "WINDOW_WinClass";
+	win32Window->hInstance = GetModuleHandle(NULL);
+	win32Window->wc.style = CS_HREDRAW | CS_VREDRAW;
+	win32Window->wc.lpfnWndProc = WINDOW_WinProc;
+	win32Window->wc.cbClsExtra = 0;
+	win32Window->wc.cbWndExtra = 0;
+	win32Window->wc.hInstance = win32Window->hInstance;
+	win32Window->wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	win32Window->wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	win32Window->wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	win32Window->wc.lpszMenuName = NULL;
+	win32Window->wc.lpszClassName = "WINDOW_WinClass";
 
-	if (!RegisterClass(&p_win->wc)) {
-		free(p_win);
+	if (!RegisterClass(&win32Window->wc)) {
+		free(win32Window);
 		return 0;
 	}
 
-	p_win->hwnd = CreateWindow(
-		p_win->wc.lpszClassName,
+	win32Window->hwnd = CreateWindow(
+		win32Window->wc.lpszClassName,
 		p_title,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		p_width, p_height,
-		NULL, NULL, p_win->hInstance, NULL
+		NULL, NULL, win32Window->hInstance, NULL
 	);
 
-	SetWindowLongPtr(p_win->hwnd, GWLP_USERDATA, (LONG_PTR)p_window);
+	SetWindowLongPtr(win32Window->hwnd, GWLP_USERDATA, (LONG_PTR)p_window);
 
-	ShowWindow(p_win->hwnd, SW_SHOW);
+	ShowWindow(win32Window->hwnd, SW_SHOW);
 
-	p_window->handle = p_win;
+	p_window->handle = win32Window;
 	p_window->width = p_width;
 	p_window->height = p_height;
 	p_window->shouldClose = 0;
@@ -222,9 +219,9 @@ static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int
 
 static void WINDOW_WindowPoll(WINDOW_Window *p_window)
 {
-	WINDOW_Win32Window *p_win;
+	WINDOW_Win32Window *win32Window;
 	MSG msg;
-	p_win = (WINDOW_Win32Window*)p_window->handle;
+	win32Window = (WINDOW_Win32Window*)p_window->handle;
 
 	while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 		TranslateMessage(&msg);
@@ -234,14 +231,14 @@ static void WINDOW_WindowPoll(WINDOW_Window *p_window)
 
 static void WINDOW_WindowDestroy(WINDOW_Window *p_window)
 {
-	WINDOW_Win32Window *p_win;
-	p_win = (WINDOW_Win32Window*)p_window->handle;
-	if (!p_win)
+	WINDOW_Win32Window *win32Window;
+	win32Window = (WINDOW_Win32Window*)p_window->handle;
+	if (!win32Window)
 		return;
 
-	DestroyWindow(p_win->hwnd);
-	UnregisterClass(p_win->wc.lpszClassName, p_win->hInstance);
-	free(p_win);
+	DestroyWindow(win32Window->hwnd);
+	UnregisterClass(win32Window->wc.lpszClassName, win32Window->hInstance);
+	free(win32Window);
 	p_window->handle = NULL;
 }
 
@@ -271,12 +268,12 @@ typedef struct WINDOW_MacWindow {
 
 static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int p_width, int p_height)
 {
-	WINDOW_MacWindow *p_mac;
-	p_mac = (WINDOW_MacWindow*)malloc(sizeof(WINDOW_MacWindow));
-	if (!p_mac) return 0;
+	WINDOW_MacWindow *macWindow;
+	macWindow = (WINDOW_MacWindow*)malloc(sizeof(WINDOW_MacWindow));
+	if (!macWindow) return 0;
 
 	Class NSApplicationClass = (Class)objc_getClass("NSApplication");
-	p_mac->nsapp = ((id(*)(Class, SEL))objc_msgSend)(NSApplicationClass, sel_registerName("sharedApplication"));
+	macWindow->nsapp = ((id(*)(Class, SEL))objc_msgSend)(NSApplicationClass, sel_registerName("sharedApplication"));
 
 	Class NSWindowClass = (Class)objc_getClass("NSWindow");
 	NSRect frame;
@@ -285,9 +282,9 @@ static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int
 	frame.size.width = p_width;
 	frame.size.height = p_height;
 
-	p_mac->nswindow = ((id(*)(Class, SEL))objc_msgSend)(NSWindowClass, sel_registerName("alloc"));
-	p_mac->nswindow = ((id(*)(id, SEL, NSRect, NSUInteger, NSUInteger, BOOL))objc_msgSend)(
-		p_mac->nswindow,
+	macWindow->nswindow = ((id(*)(Class, SEL))objc_msgSend)(NSWindowClass, sel_registerName("alloc"));
+	macWindow->nswindow = ((id(*)(id, SEL, NSRect, NSUInteger, NSUInteger, BOOL))objc_msgSend)(
+		macWindow->nswindow,
 		sel_registerName("initWithContentRect:styleMask:backing:defer:"),
 		frame,
 		1, 0, 1
@@ -295,12 +292,12 @@ static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int
 
 	CFStringRef nsTitle;
 	nsTitle = CFStringCreateWithCString(NULL, p_title, kCFStringEncodingUTF8);
-	((void(*)(id, SEL, id))objc_msgSend)(p_mac->nswindow, sel_registerName("setTitle:"), nsTitle);
+	((void(*)(id, SEL, id))objc_msgSend)(macWindow->nswindow, sel_registerName("setTitle:"), nsTitle);
 	CFRelease(nsTitle);
 
-	((void(*)(id, SEL))objc_msgSend)(p_mac->nswindow, sel_registerName("makeKeyAndOrderFront:"), p_mac->nswindow);
+	((void(*)(id, SEL))objc_msgSend)(macWindow->nswindow, sel_registerName("makeKeyAndOrderFront:"), macWindow->nswindow);
 
-	p_window->handle = p_mac;
+	p_window->handle = macWindow;
 	p_window->width = p_width;
 	p_window->height = p_height;
 	p_window->shouldClose = 0;
@@ -310,8 +307,8 @@ static int WINDOW_WindowCreate(WINDOW_Window *p_window, const char *p_title, int
 
 static void WINDOW_WindowPoll(WINDOW_Window *p_window)
 {
-	WINDOW_MacWindow *p_mac;
-	p_mac = (WINDOW_MacWindow*)p_window->handle;
+	WINDOW_MacWindow *macWindow;
+	macWindow = (WINDOW_MacWindow*)p_window->handle;
 
 	Class NSAppClass = (Class)objc_getClass("NSApplication");
 	id sharedApp = ((id(*)(Class, SEL))objc_msgSend)(NSAppClass, sel_registerName("sharedApplication"));
@@ -320,12 +317,12 @@ static void WINDOW_WindowPoll(WINDOW_Window *p_window)
 
 static void WINDOW_WindowDestroy(WINDOW_Window *p_window)
 {
-	WINDOW_MacWindow *p_mac;
-	p_mac = (WINDOW_MacWindow*)p_window->handle;
-	if (!p_mac) return;
+	WINDOW_MacWindow *macWindow;
+	macWindow = (WINDOW_MacWindow*)p_window->handle;
+	if (!macWindow) return;
 
-	((void(*)(id, SEL))objc_msgSend)(p_mac->nswindow, sel_registerName("close"));
-	free(p_mac);
+	((void(*)(id, SEL))objc_msgSend)(macWindow->nswindow, sel_registerName("close"));
+	free(macWindow);
 	p_window->handle = NULL;
 }
 
@@ -337,4 +334,3 @@ static int WINDOW_WindowShouldClose(const WINDOW_Window *p_window)
 #endif /* WINDOW_MACOS */
 
 #endif /* WINDOW_H */
-
